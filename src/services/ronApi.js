3eb0907d3d57739a2ab80ee68b5074418d96d47b
Cari Palazzolo <cari.pizza@gmail.com>
2019-03-06 09:08:13 -0800

    add production api link as commented note, for netlify

--- src/services/ronApi.js
+++ src/services/ronApi.js
@@ -1,4 +1,7 @@
 export const getFacts = count => {
+  // for dev build:
   return fetch(`http://ron-swanson-quotes.herokuapp.com/v2/quotes/${count}`)
+  // for production build:
+  // return fetch(`https://cors-anywhere.herokuapp.com/http://ron-swanson-quotes.herokuapp.com/v2/quotes/${count}`)
     .then(res => res.json());
 };
