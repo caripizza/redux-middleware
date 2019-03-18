6f50eddb62a3e7d65ea269472e89015bbd672810
Cari Palazzolo <cari.pizza@gmail.com>
2019-03-18 07:18:44 -0700

    tweak for netlify cors issue

--- src/services/ronApi.js
+++ src/services/ronApi.js
@@ -1,7 +1,13 @@
 export const getFacts = count => {
   // for dev build:
-  return fetch(`http://ron-swanson-quotes.herokuapp.com/v2/quotes/${count}`)
+  // return fetch(`http://ron-swanson-quotes.herokuapp.com/v2/quotes/${count}`)
   // for production build:
-  // return fetch(`https://cors-anywhere.herokuapp.com/http://ron-swanson-quotes.herokuapp.com/v2/quotes/${count}`)
+  return fetch(`https://cors-anywhere.herokuapp.com/http://ron-swanson-quotes.herokuapp.com/v2/quotes/${count}`, { mode: 'cors' })
     .then(res => res.json());
 };
+
+// module.exports = (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', '*');
+//   next();
+// };
